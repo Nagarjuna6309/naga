6dfa9b0c86ef3e3051bddba2cd8a2d144d75d29c
Nagarjuna6309 <brocklesnarnaga1111@gmail.com>
2022-11-21 20:23:45 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def naga(request):
+    return HttpResponse('<h1><marquee>hii my name NAGARJUNA</marquee></h1>')
+
+# Create your views here.
